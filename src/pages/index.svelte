934f5aa37a91d3ed0fa65e1aa7ae00ff4456bdd6
Antonio Bourassa <abourassa@assetval.com>
2021-03-05 19:48:43 -0700

    :tada: Make it so number one

---
+++ src/pages/index.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Logo from './example/_components/Logo.svelte';
+  import {metatags} from '@roxi/routify'
+  import { url } from "@roxi/routify";
+
+  metatags.title = 'The Modern Farm - Grow Ideas'
+  metatags.description = 'The Modern Farm - Grow Ideas'
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
+
+<div class="bg-gray-100 dark:bg-white-800">
+  <Logo />
+</div>
+<div class="mx-auto">
+  <a href={$url('/example')}>Back to examples</a>
+ <span>Sample Content</span>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const production = !process.env.ROLLUP_WATCH;
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  plugins: [],
+  darkMode: 'media', // or 'media' or 'class'
+  purge: {
+    content: ["./src/**/*.svelte"],
+    enabled: production,
+  },
+};
